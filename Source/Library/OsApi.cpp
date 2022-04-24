78337c481735d275389e9bdf9e6e63d90033ad3b
sylwow <Sylwo1003@gmail.com>
2022-04-24 13:27:37 +0200

    change controlls to wasd

--- Source/Library/OsApi.cpp
+++ Source/Library/OsApi.cpp
@@ -11,12 +11,12 @@
 
 #ifdef linux
 
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <termios.h>
-#include <iostream>
+#include <unistd.h>
 
 #define LINUX
 #endif
@@ -28,19 +28,19 @@ namespace
 
     Key getCurrentKeyPressedOs()
     {
-        if ((1 << 15) & GetAsyncKeyState(VK_UP))
+        if ((1 << 15) & GetAsyncKeyState('W'))
         {
             return Key::Up;
         }
-        if ((1 << 15) & GetAsyncKeyState(VK_DOWN))
+        if ((1 << 15) & GetAsyncKeyState('S'))
         {
             return Key::Down;
         }
-        if ((1 << 15) & GetAsyncKeyState(VK_LEFT))
+        if ((1 << 15) & GetAsyncKeyState('A'))
         {
             return Key::Left;
         }
-        if ((1 << 15) & GetAsyncKeyState(VK_RIGHT))
+        if ((1 << 15) & GetAsyncKeyState('D'))
         {
             return Key::Right;
         }
@@ -55,57 +55,53 @@ namespace
 #endif
 
 #ifdef LINUX
-    void initOs() {
+    void initOs() {}
 
-    }
-
-    char getch() {
+    char getch()
+    {
         char buf = 0;
         struct termios old = {0};
         if (tcgetattr(0, &old) < 0)
-                perror("tcsetattr()");
+            perror("tcsetattr()");
         old.c_lflag &= ~ICANON;
         old.c_lflag &= ~ECHO;
         old.c_cc[VMIN] = 0;
         old.c_cc[VTIME] = 0;
         if (tcsetattr(0, TCSANOW, &old) < 0)
-                perror("tcsetattr ICANON");
+            perror("tcsetattr ICANON");
         if (read(0, &buf, 1) < 0)
-                perror ("read()");
+            perror("read()");
         old.c_lflag |= ICANON;
         old.c_lflag |= ECHO;
         if (tcsetattr(0, TCSADRAIN, &old) < 0)
-                perror ("tcsetattr ~ICANON");
-        
+            perror("tcsetattr ~ICANON");
+
         return (buf);
     }
 
     Key getCurrentKeyPressedOs()
     {
-        int ch = getch(); 
-        if (ch == 'w')  
+        int ch = getch();
+        if (ch == 'w')
         {
             return Key::Up;
         }
-        if (ch == 's')  
+        if (ch == 's')
         {
             return Key::Down;
         }
-        if (ch == 'a')  
+        if (ch == 'a')
         {
             return Key::Left;
         }
-        if (ch == 'd')  
+        if (ch == 'd')
         {
             return Key::Right;
         }
         return Key::None;
     }
 
-    void clearConsoleOs()
-    {
-        printf("\033[2J\033[1;1H");
-    }
+    void clearConsoleOs() { printf("\033[2J\033[1;1H"); }
 #endif
 
 } // namespace
