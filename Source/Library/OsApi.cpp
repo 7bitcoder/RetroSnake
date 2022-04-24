10ca59817340f378f0411f9ae74935eb28797ec7
Sylwow-Ubuntu <Sylwo1003@gmail.com>
2022-04-24 13:24:51 +0200

    finish linux implementation

--- Source/Library/OsApi.cpp
+++ Source/Library/OsApi.cpp
@@ -15,6 +15,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <termios.h>
+#include <iostream>
 
 #define LINUX
 #endif
@@ -57,25 +59,46 @@ namespace
 
     }
 
+    char getch() {
+        char buf = 0;
+        struct termios old = {0};
+        if (tcgetattr(0, &old) < 0)
+                perror("tcsetattr()");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 0;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &old) < 0)
+                perror("tcsetattr ICANON");
+        if (read(0, &buf, 1) < 0)
+                perror ("read()");
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+        if (tcsetattr(0, TCSADRAIN, &old) < 0)
+                perror ("tcsetattr ~ICANON");
+        
+        return (buf);
+    }
+
     Key getCurrentKeyPressedOs()
     {
-        int ch = getchar(); 
-        // if (ch == KEY_UP)  
-        // {
-        //     return Key::Up;
-        // }
-        // if (ch == KEY_DOWN)  
-        // {
-        //     return Key::Down;
-        // }
-        // if (ch == KEY_LEFT)  
-        // {
-        //     return Key::Left;
-        // }
-        // if (ch == KEY_RIGHT)  
-        // {
-        //     return Key::Right;
-        // }
+        int ch = getch(); 
+        if (ch == 'w')  
+        {
+            return Key::Up;
+        }
+        if (ch == 's')  
+        {
+            return Key::Down;
+        }
+        if (ch == 'a')  
+        {
+            return Key::Left;
+        }
+        if (ch == 'd')  
+        {
+            return Key::Right;
+        }
         return Key::None;
     }
 
